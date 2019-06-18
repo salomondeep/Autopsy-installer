969cc8873fea61e2e310efeb1f3e273b8653ae85
Marin Grabovschi <marin.grabovschi@gmail.com>
2019-06-18 17:58:13 +0100

    Code refactoring and tested successfully

--- autopsy-script.1.pl
+++ autopsy-script.1.pl
@@ -20,48 +20,70 @@ my $upgrade = "sudo apt-get upgrade";
 my $install_unzip_command = "sudo apt-get install unzip";
 my $install_git_command = "sudo apt-get install git";
 my $install_curl_command = "sudo apt-get install curl";
+my $install_testdisk_command = "sudo apt-get install testdisk";
 my $install_gcc_command = "sudo apt-get install build-essential aptitude libstdc++6 gcc";
+
 my $java_installer_script = "git clone https://github.com/chrishantha/install-java.git";
 my $check_unzip = "(dpkg-query -W -f='\${Status}' unzip 2>/dev/null | grep -c \"ok installed\")";
 my $check_java = "(dpkg-query -W -f='\${Status}' java 2>/dev/null | grep -c \"ok installed\")";
 my $check_git = "(dpkg-query -W -f='\${Status}' git 2>/dev/null | grep -c \"ok installed\")";
 my $check_curl = "(dpkg-query -W -f='\${Status}' curl 2>/dev/null | grep -c \"ok installed\")";
-
+my $check_testdisk = "(dpkg-query -W -f='\${Status}' testdisk 2>/dev/null | grep -c \"ok installed\")";
 my $find_jdk = "find /home -name \"jdk*.tar.gz\" -size +20M | tr -d '\r\n'";
 #----------------------------------------------------------------------------------------
 
 
-my $update_output = system($update);
-my $upgrade_output = system($upgrade);
+# my $update_output = system($update);
+# my $upgrade_output = system($upgrade);
 
 
-#---------------------- CHECK REQUIRED PACKAGES ------------------------------------------------------------------
+# #---------------------- CHECK REQUIRED PACKAGES ------------------------------------------------------------------
 
-my $check_unzip_output = `$check_unzip`;
-if ($check_unzip_output == 0) ? system($install_unzip_command) : print "unzip is installed";
+# my $check_unzip_output = `$check_unzip`;
+# if ($check_unzip_output == 0) {
+#     system($install_unzip_command);
+# }else{
+#     print "unzip is installed";
+# }
 
-my $check_curl_output = `$check_curl`;
-if ($check_curl_output == 0) ? system($install_curl_command) : print "curl is installed";
+# my $check_curl_output = `$check_curl`;
+# if ($check_curl_output == 0) {
+#     system($install_curl_command);
+# }else{
+#     print "curl is installed";
+# }
 
-my $check_git_output = `$check_git`;
-if ($check_git_output == 0) ? system($install_git_command) : print "git is installed";
+# my $check_git_output = `$check_git`;
+# if ($check_git_output == 0) {
+#     system($install_git_command);
+# }else{
+#     print "git is installed";
+# }
 
-system($install_gcc_command);
+# system($install_gcc_command);
 
-my $check_java_output = `$check_java`;
+# my $check_testdisk_output = `$check_testdisk`;
+# if ($check_testdisk_output == 0) {
+#     system($install_testdisk_command)
+# }else{
+#     print "testdisk is installed";
+# }
+
+my $check_java_output = `$check_java`; print $check_java_output;
 if ($check_java_output == 0){
     #install here java
-    system($java_installer_script);
+    system($java_installer_script); #works
 
     #step 1 -> find where is jdk and store its path
-    my $find_jdk_output = `$find_jdk`;
+    my $find_jdk_output = `$find_jdk`; #print $find_jdk_output; #works
 
     #step 2 -> copy the jdk to the directory of the 
-    my $aux = "cp ". $find_jdk_output ." ./install-java";
+    my $aux = "cp ". $find_jdk_output ." ./install-java"; #works
     system($aux);
 
-    #step 3 -> move to the directory and execute the shell script
-    my $execute_script = "cd install-java/ && ./install-java -f "
+    #step 3 -> move to the directory and execute the shell script #works
+    my $execute_script = "cd install-java/ && ./install-java.sh -f jdk-8u212-linux-x64.tar.gz && source ~/.bashrc && echo \$JAVA_HOME";
+    system($execute_script);
 
 } else{
     print "java is installed";
